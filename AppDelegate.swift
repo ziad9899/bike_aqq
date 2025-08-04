2c27a0d56389a0b0f9f55170e7da1512f0118674
ziad9899 <alofi9899@gmail.com>
2025-08-04 19:55:33 +0300

    Add files via upload

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,13 +1,13 @@
-import Cocoa
-import FlutterMacOS
+import Flutter
+import UIKit
 
 @main
-class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
